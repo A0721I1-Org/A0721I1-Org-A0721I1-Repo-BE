2ded1b5b910cb0de1a4c66ca06d8baa44314f88c
thanhtu2394 <thieuthanhtu1994@gmail.com>
2022-06-06 21:07:29 +0700

    up demo

--- src/main/java/projecta07/service/ITypeProductService.java
+++ src/main/java/projecta07/service/ITypeProductService.java
@@ -1,4 +1,9 @@
 package projecta07.service;
 
-public interface ITypeProductService {
+import projecta07.model.TypeProduct;
+
+import java.util.List;
+
+public interface ITypeProductService  {
+    List<TypeProduct> findByAll();
 }
--- src/main/java/projecta07/service/impl/TypeProductService.java
+++ src/main/java/projecta07/service/impl/TypeProductService.java
@@ -1,8 +1,19 @@
 package projecta07.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import projecta07.model.TypeProduct;
+import projecta07.repository.ITypeProductRepository;
 import projecta07.service.ITypeProductService;
+
+import java.util.List;
 
 @Service
 public class TypeProductService implements ITypeProductService {
+    @Autowired
+    ITypeProductRepository iTypeProductRepository;
+    @Override
+    public List<TypeProduct> findByAll() {
+        return iTypeProductRepository.findAll();
+    }
 }
