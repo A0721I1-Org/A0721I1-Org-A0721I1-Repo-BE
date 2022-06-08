30378745d12187ec82c002fe893ac6c4748ed990
HIEU DINH <dvhieu.espd.ufl@gmail.com>
2022-06-08 18:08:42 +0700

    HieuDV - xem va tim kiem danh sach phan hoi

--- src/main/java/projecta07/controller/FeedbackController.java
+++ src/main/java/projecta07/controller/FeedbackController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/feedback")
+@RequestMapping("manager/api/feedback")
 public class FeedbackController {
 
     @Autowired
