692cc1e5449c8367a2b8a470f6e601cd61363af1
HIEU DINH <dvhieu.espd.ufl@gmail.com>
2022-06-08 18:03:09 +0700

    HieuDv -xem va tim kiem  danh sach quan ly feedback

--- src/main/java/projecta07/controller/FeedbackController.java
+++ src/main/java/projecta07/controller/FeedbackController.java
@@ -1,7 +1,50 @@
 package projecta07.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import projecta07.model.Feedback;
+import projecta07.service.impl.FeedbackService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/feedback")
 public class FeedbackController {
+
+    @Autowired
+    private FeedbackService feedbackService;
+
+    @GetMapping("/")
+    public ResponseEntity<Iterable<Feedback>> findAllFeedback() {
+        List<Feedback> feedbackList = feedbackService.findAll();
+        if (feedbackList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(feedbackList,HttpStatus.OK);
+    }
+
+    @GetMapping("/search/{date}")
+    public ResponseEntity<Iterable<Feedback>> findAllFeedbackByDateFeedback(@PathVariable String date) {
+        String dateSearch = date.replace("-","/");
+        List<Feedback> feedbackListByDate = feedbackService.findAllFeedbackByDateFeedback(dateSearch);
+        if (feedbackListByDate.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(feedbackListByDate,HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Feedback> findFeedbackById(@PathVariable Long id) {
+        Optional<Feedback> feedbackOptional = feedbackService.findFeedbackById(id);
+        if (!feedbackOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(feedbackOptional.get(), HttpStatus.OK);
+    }
 }
--- src/main/java/projecta07/service/impl/FeedbackService.java
+++ src/main/java/projecta07/service/impl/FeedbackService.java
@@ -1,8 +1,33 @@
 package projecta07.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import projecta07.model.Feedback;
+import projecta07.repository.IFeedbackRepository;
 import projecta07.service.IFeedbackService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FeedbackService implements IFeedbackService {
+    @Autowired
+    private IFeedbackRepository iFeedbackRepository;
+
+    @Override
+    public List<Feedback> findAll() {
+        return iFeedbackRepository.findAll();
+    }
+
+    @Override
+    public List<Feedback> findAllFeedbackByDateFeedback(String date) {
+        return iFeedbackRepository.findAllFeedbackByDateFeedback(date);
+    }
+
+    @Override
+    public Optional<Feedback> findFeedbackById(Long id) {
+        return iFeedbackRepository.findById(id);
+    }
 }
