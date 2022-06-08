c62d7b149eaf0e00c5c69858d017a5301e163f60
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-08 16:49:03 +0700

    show view menu-TuanNHA

--- src/main/java/projecta07/service/impl/OrderDetailService.java
+++ src/main/java/projecta07/service/impl/OrderDetailService.java
@@ -1,8 +1,21 @@
 package projecta07.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import projecta07.model.OrderDetail;
+import projecta07.repository.IOrderDetailRepository;
 import projecta07.service.IOrderDetailService;
+
+import java.util.List;
 
 @Service
 public class OrderDetailService implements IOrderDetailService {
+    @Autowired
+    private IOrderDetailRepository orderDetailRepository;
+
+    /* get Order Detail by Order Id*/
+    public List<OrderDetail> getOrdersDetail(Long id) {
+        return this.orderDetailRepository.getOrderDetailsByOrderId(id);
+
+    }
 }
