6d2e741fd27895250855f958d7dc4d7717ec01a6
HIEU DINH <dvhieu.espd.ufl@gmail.com>
2022-06-08 21:07:30 +0700

    HieuDV update

--- src/main/java/projecta07/controller/FeedbackController.java
+++ src/main/java/projecta07/controller/FeedbackController.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("manager/api/feedback")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FeedbackController {
 
     @Autowired
