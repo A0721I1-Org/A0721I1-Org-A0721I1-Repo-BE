ba2befc97a659d628830b97a1c750bd1accf5f63
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-15 17:13:38 +0700

    fix button payment and remove attribute status_order

--- src/main/java/projecta07/repository/ITableRepository.java
+++ src/main/java/projecta07/repository/ITableRepository.java
@@ -1,6 +1,7 @@
 package projecta07.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import projecta07.model.Table;
 
