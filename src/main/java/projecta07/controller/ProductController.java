f55fbd2eff29081e9a387635cd097a65fc82a976
thanhtu2394 <thieuthanhtu1994@gmail.com>
2022-06-20 20:29:21 +0700

    update

--- src/main/java/projecta07/controller/ProductController.java
+++ src/main/java/projecta07/controller/ProductController.java
@@ -37,7 +37,7 @@ public class ProductController {
     @GetMapping("/page")
     public ResponseEntity<Page<Product>> findByAllPageGing( Pageable pageable,@RequestParam String page){
         int page1= Integer.parseInt(page);
-        pageable =PageRequest.of(page1,4);
+        pageable =PageRequest.of(page1,6);
         Page<Product> productList1=productService.findByAllPaging(pageable);
         if (productList1.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -73,7 +73,7 @@ public class ProductController {
                                                 @RequestParam(defaultValue = "")String name,
                                                 Pageable pageable,@RequestParam String page1){
         int page2= Integer.parseInt(page1);
-        pageable =PageRequest.of(page2,4);
+        pageable =PageRequest.of(page2,6);
         Page<Product> productList2=productService.searchPage(code,name,pageable);
         if (productList2.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
