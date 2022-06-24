5924bea2b5f1f6a3eeee5827da0cef3feb97f29f
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-24 12:57:35 +0700

    fix

--- src/main/java/projecta07/dto/FeedbackDTO.java
+++ src/main/java/projecta07/dto/FeedbackDTO.java
@@ -4,15 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import projecta07.service.IFeedbackService;
 
+import javax.persistence.Column;
 import javax.validation.Valid;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Getter
@@ -25,18 +24,22 @@ public class FeedbackDTO  implements  Validator{
     private String codeFeedback;
     private LocalDate dateFeedback;
 
-    @NotBlank
-    @Size(min = 3, max = 100)
+    @NotEmpty(message = "cannot be empty")
+    @Length(min = 3 , max = 50 , message = "must be between 3 and 50")
+    @Column(name = "content_feedback")
     private String contentFeedback;
 
+    @NotEmpty(message = "cannot be empty")
     @Pattern(regexp = "^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s\\W|_]+", message = "Định dạng tên không đúng")
-    @Size(min = 3, max = 50)
-    @NotBlank
+    @Length(min = 3 , max = 50 , message = "must be between 3 and 50")
+    @Column(name = "name_people_feedback")
     private String namePeopleFeedback;
 
-    @NotBlank
+    @NotEmpty
     @Email
+    @Column(name = "email_people_feedback")
     private String emailPeopleFeedback;
+
     private String imageFeedback;
 
     @Valid
@@ -50,5 +53,69 @@ public class FeedbackDTO  implements  Validator{
     @Override
     public void validate(Object target, Errors errors) {
 
+    }
+
+    public Long getIdFeedback() {
+        return idFeedback;
+    }
+
+    public void setIdFeedback(Long idFeedback) {
+        this.idFeedback = idFeedback;
+    }
+
+    public String getCodeFeedback() {
+        return codeFeedback;
+    }
+
+    public void setCodeFeedback(String codeFeedback) {
+        this.codeFeedback = codeFeedback;
+    }
+
+    public LocalDate getDateFeedback() {
+        return dateFeedback;
+    }
+
+    public void setDateFeedback(LocalDate dateFeedback) {
+        this.dateFeedback = dateFeedback;
+    }
+
+    public String getContentFeedback() {
+        return contentFeedback;
+    }
+
+    public void setContentFeedback(String contentFeedback) {
+        this.contentFeedback = contentFeedback;
+    }
+
+    public String getNamePeopleFeedback() {
+        return namePeopleFeedback;
+    }
+
+    public void setNamePeopleFeedback(String namePeopleFeedback) {
+        this.namePeopleFeedback = namePeopleFeedback;
+    }
+
+    public String getEmailPeopleFeedback() {
+        return emailPeopleFeedback;
+    }
+
+    public void setEmailPeopleFeedback(String emailPeopleFeedback) {
+        this.emailPeopleFeedback = emailPeopleFeedback;
+    }
+
+    public String getImageFeedback() {
+        return imageFeedback;
+    }
+
+    public void setImageFeedback(String imageFeedback) {
+        this.imageFeedback = imageFeedback;
+    }
+
+    public IFeedbackService getFeedbackService() {
+        return feedbackService;
+    }
+
+    public void setFeedbackService(IFeedbackService feedbackService) {
+        this.feedbackService = feedbackService;
     }
 }
